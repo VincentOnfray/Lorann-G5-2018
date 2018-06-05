84a515c8c151708c37bea482eac63dd685e3efa2
Kery971 <33548903+Kery971@users.noreply.github.com>
2018-06-05 11:24:43 +0200

    Revert "Merge branch 'master' of https://github.com/VincentOnfray/Lorann-G5-2018"
    This reverts commit dd590b613a34857d93501a5710c8c3aa918ef8f2, reversing
    changes made to 6e335f89ed50b5fba01f80923d78316332a1af3e.

---
+++ Lorann/model/test/FactoryTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FactoryTest {
+	
+	newDemon Demon = new Demon();
+	int 
+	
+	@Test
+	public void testnewDemon(8,14,1) {
+		assertEquals();
+	}
+
+}
