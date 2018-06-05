9bbc7e6febedb7d3ac6dea6236c3ea0793064fc8
Loris <33549100+LorisJemmi@users.noreply.github.com>
2018-06-05 10:05:18 +0200

    test

---
+++ Lorann/model/test/MobileElementTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MobileElementTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
