60b88650eeaca5961fa433da94464b970092a88b
Kery971 <33548903+Kery971@users.noreply.github.com>
2018-06-05 14:44:59 +0200

    Test Fini

--- Lorann/view/src/test/java/view/PanelTest.java
+++ Lorann/view/src/test/java/view/PanelTest.java
@@ -2,13 +2,31 @@ package view;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
+import view.Panel;
+import model.Element;
+import model.Ground;
 
 public class PanelTest {
 
 	@Test
 	public void test() {
-		;
+		Element[][] map = new Element[20][13];
+		 for(int x = 0; x < 20; x++) {
+		        for(int y = 0; y< 13; y++) {
+		        	try {
+						map[x][y] = new Ground();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        }
+		    }
+		Panel panel = new Panel( map);
+		
+		assertNotNull("panel non null", panel);
 	}
 
 }
