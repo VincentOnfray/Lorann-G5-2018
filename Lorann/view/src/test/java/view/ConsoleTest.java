dd06931a040d61bf5b991cb19da7273eff5081ab
Kery971 <33548903+Kery971@users.noreply.github.com>
2018-06-05 14:29:09 +0200

    Robot Achievement

--- Lorann/view/src/test/java/view/ConsoleTest.java
+++ Lorann/view/src/test/java/view/ConsoleTest.java
@@ -1,14 +1,23 @@
 package view;
 
 import static org.junit.Assert.*;
-
+import view.Console;
 import org.junit.Test;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.*;
 public class ConsoleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	public void test() throws AWTException {
+		
+		Console console = new Console();
+		Robot robot = new Robot();
+	
+		robot.keyPress(KeyEvent.VK_NUMPAD1);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		assertNotNull(console.whatMap());
+		
+		}
 }
--- Lorann/view/src/test/java/view/PanelTest.java
+++ Lorann/view/src/test/java/view/PanelTest.java
@@ -8,7 +8,7 @@ public class PanelTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		;
 	}
 
 }
